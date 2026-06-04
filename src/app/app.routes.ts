be3f17fec10477e75639d2ce54a934221a2d5ce5
fbansept <bansept.franck@gmail.com>
2026-06-04 11:59:48 +0200

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { Connexion } from './pages/connexion/connexion';
+import { Page404 } from './pages/page404/page404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'accueil', component: Accueil },
+  { path: 'connexion', component: Connexion },
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: '**', component: Page404 },
+];
