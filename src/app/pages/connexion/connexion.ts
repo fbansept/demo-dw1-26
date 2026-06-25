0ed1cbc8cfdd6ca0a4869a5292108a72b0fad18f
fbansept <bansept.franck@gmail.com>
2026-06-25 09:40:10 +0200

    formulaire

--- src/app/pages/connexion/connexion.ts
+++ src/app/pages/connexion/connexion.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
-  imports: [MatInputModule, MatButtonModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule, 
+    MatButtonModule
+  ],
   templateUrl: './connexion.html',
   styleUrl: './connexion.scss',
 })
-export class Connexion {}
+export class Connexion {
+
+  formBuilder = inject(FormBuilder)
+  formulaire = this.formBuilder.group(
+    {
+      email : ["",[Validators.email, Validators.required]],
+      password : ["",[Validators.required]]
+    }
+  )
+
+}
